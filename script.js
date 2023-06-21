67ad5ab9afb3e1b10bcc06275e3dcc0a27ad464e
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-06-21 15:40:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const div = document.getElementById("browser-info");
+
+
+      const browserName = navigator.userAgent;
+      const browserVersion = navigator.appVersion;
+      
+      const message = `You are using ${browserName} (version ${browserVersion}).`;
+      
+      document.getElementById("browser-info").textContent = message;
+div.append(message);
